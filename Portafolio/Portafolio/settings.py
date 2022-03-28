9ff27b0c1168b4d37f9fd3d7067f369f6d965887
EFREN <efrennm86@gmail.com>
2022-03-27 20:51:36 -0600

    SOLUCIONADO PROBLEMA IMG EN ADMIN. CAMBIOS EN SETTINGS, URLS

--- Portafolio/Portafolio/settings.py
+++ Portafolio/Portafolio/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -119,9 +120,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-MEDIA_ROOT = BASE_DIR / 'media'
-
-MEDIA_URL = '/public/'
+MEDIA_URL ='/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
